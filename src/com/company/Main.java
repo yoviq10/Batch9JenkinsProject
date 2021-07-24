8244f39ff72062e6a8b88f4e74eda382d3c7ec54
yoviq10 <quisbert.yovanna@gmail.com>
2021-07-24 00:05:05 -0400

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 6; i++) {
             System.out.println("Batch 9 is great");
             Thread.sleep(1000);
         }
