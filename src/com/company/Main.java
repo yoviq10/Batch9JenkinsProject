dd9dc8134a7cc513352c61ffc1772b3cf68f94ef
yoviq10 <quisbert.yovanna@gmail.com>
2021-07-23 23:48:33 -0400

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Batch 9 is great");
+            Thread.sleep(1000);
+        }
+
+
+    }
+
+}
